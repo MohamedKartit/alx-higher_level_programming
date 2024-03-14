62d6081cf17c1d05a6cbf7dba2df32909434c9c6
Mohamed Kartit <mkartit9@gmail.com>
2024-03-14 04:25:53 -0700

    add gitignore

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add as perf_addd
+    from add_0 import add
     a = 1
     b = 2
-    result = perf_addd(a, b)
-    print(f"{a:d} + {b:d} = {result:d}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
