a79fcac3f42903873d7760ceef72123a5eb3b8a7
Mohamed Kartit <mkartit9@gmail.com>
2024-05-21 06:21:11 -0700

    Add tasks 6 and 7

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- lists row of the table from database in the server.
+SELECT * FROM `first_table`;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Insert new row in table of a database in the server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
