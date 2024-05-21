37a84788c5c62f3fa2e01194d7ba3e51fba8769b
Mohamed Kartit <mkartit9@gmail.com>
2024-05-21 07:19:22 -0700

    Add tasks 12, 13 and 14

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes all record where score <= 5 in a table of database in a server.
+DELETE FROM `second_table`
+WHERE `score` <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- Computes the avarage of all records in a table of a database in a server.
+SELECT AVG(`score`) AS `average` FROM `second_table`;
